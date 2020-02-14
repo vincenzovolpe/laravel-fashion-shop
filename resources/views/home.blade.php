2e3fe64edeaf0dd547c7af45347dd2de7ab7692b
vincenzovolpe <vinci792010@gmail.com>
2020-02-14 09:17:52 +0100

    Aggiunta nuove schede con più dati (sa rendere dinamici)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,126 @@
         </div>
       </section>
 
+      <div class="container">
+      <h3 class="h3">Nuovi Arrivi</h3>
+      <div class="row">
+          <div class="col-md-3 col-sm-6">
+              <div class="product-grid3">
+                  <div class="product-image3">
+                      <a href="#">
+                          <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-1.jpg">
+                          <img class="pic-2" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-2.jpg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
+                          <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                      </ul>
+                      <span class="product-new-label">New</span>
+                  </div>
+                  <div class="product-content">
+                      <h3 class="title"><a href="#">Men's Blazer</a></h3>
+                      <div class="price">
+                          $63.50
+                          <span>$75.00</span>
+                      </div>
+                      <ul class="rating">
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star disable"></li>
+                          <li class="fa fa-star disable"></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+              <div class="product-grid3">
+                  <div class="product-image3">
+                      <a href="#">
+                          <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-3.jpg">
+                          <img class="pic-2" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-4.jpg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
+                          <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                      </ul>
+                  </div>
+                  <div class="product-content">
+                      <h3 class="title"><a href="#">Women's Designer Top</a></h3>
+                      <div class="price">
+                          $43.50
+                      </div>
+                      <ul class="rating">
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+              <div class="product-grid3">
+                  <div class="product-image3">
+                      <a href="#">
+                          <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-5.jpg">
+                          <img class="pic-2" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-6.jpg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
+                          <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                      </ul>
+                      <span class="product-new-label">New</span>
+                  </div>
+                  <div class="product-content">
+                      <h3 class="title"><a href="#">Men's Blazer</a></h3>
+                      <div class="price">
+                          $63.50
+                          <span>$75.00</span>
+                      </div>
+                      <ul class="rating">
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star disable"></li>
+                          <li class="fa fa-star disable"></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+              <div class="product-grid3">
+                  <div class="product-image3">
+                      <a href="#">
+                          <img class="pic-1" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-7.jpg">
+                          <img class="pic-2" src="http://bestjquery.com/tutorial/product-grid/demo4/images/img-8.jpg">
+                      </a>
+                      <ul class="social">
+                          <li><a href="#"><i class="fa fa-shopping-bag"></i></a></li>
+                          <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                      </ul>
+                      <span class="product-new-label">New</span>
+                  </div>
+                  <div class="product-content">
+                      <h3 class="title"><a href="#">Men's Blazer</a></h3>
+                      <div class="price">
+                          $63.50
+                          <span>$75.00</span>
+                      </div>
+                      <ul class="rating">
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star"></li>
+                          <li class="fa fa-star disable"></li>
+                          <li class="fa fa-star disable"></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
       <div class="album py-5 bg-light">
         <div class="container">
           <div class="row row-cols-1 row-cols-md-3">
@@ -21,13 +141,15 @@
                       <div class="card-body">
                         <h5 class="card-title">{{$cloth->name}}</h5>
                         <p class="card-text text-justify">{{$cloth->description}}</p>
+                        <p class="card-text">€ {{$cloth->price}}</p>
                       </div>
                     </div>
                   </div>
               @endforeach
           </div>
         </div>
-      </div>
 
+  </div>
+      </div>
     </main>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>Boolean.Careers</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     </head>
     <body>
         @include('layouts.header')
